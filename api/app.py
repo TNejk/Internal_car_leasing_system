ead2f635cf6821eadb53cb1ef6d159cee741e0a5
Devlogs <102648064+IndexProselyte@users.noreply.github.com>
2024-11-28 12:17:41 +0100

    Update app.py

--- api/app.py
+++ api/app.py
@@ -217,7 +217,7 @@ def lease_car():
   # USER CHECKER 
   cur.execute("select * from driver where email = %s and role = %s", (username, role,))
   user = cur.fetchall()
-  return jsonify(msg= user)
+  return jsonify(msg= f"{user}, {cur}")
   user_id = user[0]
 
   cur.execute("select id from car where name = %s", (car_name,))
