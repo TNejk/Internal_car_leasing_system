3bd66288eea178ca2d629cbebe24adce346ff8b1
Devlogs <102648064+IndexProselyte@users.noreply.github.com>
2024-12-13 18:21:12 +0100

    Update app.py

--- api/app.py
+++ api/app.py
@@ -260,35 +260,33 @@ def get_full_car_info():
     cur.execute(query, (car, True, ))
     resu = cur.fetchall()
 
-    # [(datetime(2024, 12, 15, 12, 0), datetime(2024, 12, 20, 12, 0))]
-    # res: ["(\"2024-12-08 10:06:53.294548+01\",\"2024-12-08 11:06:53.294554+01\")"]
-    # dates = []
-    # def parse_lease_dates(resu):
-    #   lease_dates = []
-    #   for date_range in resu:
-    #       # Parse the strings into datetime objects
-    #       start_datetime = datetime.fromisoformat(date_range[0])
-    #       end_datetime = datetime.fromisoformat(date_range[1])
+    dates = []
+    def parse_lease_dates(resu):
+      lease_dates = []
+      for date_range in resu:
+          # Parse the strings into datetime objects
+          start_datetime = datetime.fromisoformat(date_range[0])
+          end_datetime = datetime.fromisoformat(date_range[1])
           
-    #       lease_dates.append((start_datetime, end_datetime))
-    #   return lease_dates
+          lease_dates.append((start_datetime, end_datetime))
+      return lease_dates
 
-    # # Parse the dates
-    # lease_dates = parse_lease_dates(resu)
-    # dates = filter_dates(lease_dates)
+    # Parse the dates
+    lease_dates = parse_lease_dates(resu)
+    dates = filter_dates(lease_dates)
     
-    # if resu:  
-    #    for i in range(0, len(resu)):
-    #      try:
-    #        dates.append((parse(resu[i][0]), parse(resu[i][1])))
-    #      except Exception as e:
-    #        return jsonify(msg= f"date error: {e}"), 500
+    if resu:  
+       for i in range(0, len(resu)):
+         try:
+           dates.append((parse(resu[i][0]), parse(resu[i][1])))
+         except Exception as e:
+           return jsonify(msg= f"date error: {e}"), 500
 
-    #    try:
-    #      #[(datetime_from, datetime_to)]
-    #      filter_dates(dates)
-    #    except Exception as e:
-    #      return jsonify(msg= f"Erorr date filtering: {e}"), 500
+       try:
+         #[(datetime_from, datetime_to)]
+         filter_dates(dates)
+       except Exception as e:
+         return jsonify(msg= f"Erorr date filtering: {e}"), 500
 
     response = jsonify({"car_details": res, "allowed_dates": []})
     return response, 200
