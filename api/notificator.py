9b0eefcff4eca5a8d466ec4dafa0cc9469ed3043
Devlogs <102648064+IndexProselyte@users.noreply.github.com>
2025-01-09 12:19:59 +0100

    Update notificator.py

--- api/notificator.py
+++ api/notificator.py
@@ -13,7 +13,7 @@ db_pass = os.getenv('POSTGRES_PASS')
 db_name = os.getenv('POSTGRES_DB')
 
 # Login to firebase    # FIREBASE
-cred = credentials.Certificate("icls-56e37-firebase-adminsdk-2d4e2-be93ca6a35.json")
+cred = credentials.Certificate("api/icls-56e37-firebase-adminsdk-2d4e2-be93ca6a35.json")
 firebase_admin.initialize_app(cred)
 
 db_con = psycopg2.connect(dbname=db_name, user=db_user, host=db_host, port=db_port, password=db_pass)
@@ -30,7 +30,6 @@ lease_query = """SELECT *
 
 cur.execute(lease_query, (datetime.now()))
 active_leases = cur.fetchall()
-
 # if its over the limit get user email
 for i in active_leases:
     email_query = "SELECT email FROM USER WHERE id_user = %s"
@@ -39,10 +38,23 @@ for i in active_leases:
 
     # send notif to the email topic and the
     str_mess = "Skončil sa limit na vrátenie auta, prosím odovzdajte auto v aplikácií!"
+
+    #TODO: PRED POSLANIM SA MUSIM ZBAVIT @ V EMAILE KEDZE TO NENI PLATNY TOPIC, NAHRAD HO _
+    #! email.replace("@", "_")
     message = messaging.Message(
                         notification=messaging.Notification(
-                        title="Nezabudni vrátiť auto!",
+                        title="Prekrocenie limitu na odovzdanie auta",
                         body=str_mess
                     ),
-                        topic=email
+                        topic=email[0].replace("@", "_")
                     )
+    
+    messaging.send(message)
+
+    message = messaging.Message(
+        notification=messaging.Notification(
+            title="Zamestnanec {} neskoro odovzdal auto {}.",
+            body="Zamestnanec {} neskoro odovzdal auto {} o {} minút."
+        ),
+        topic = "manager"
+    )
