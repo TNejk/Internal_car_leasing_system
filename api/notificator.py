9c032cfdf071b508eeab61ad6c4bee5115cc0f68
Devlogs <102648064+IndexProselyte@users.noreply.github.com>
2025-01-09 12:51:29 +0100

    Update notificator.py

--- api/notificator.py
+++ api/notificator.py
@@ -5,6 +5,7 @@ import psycopg2
 import firebase_admin
 from firebase_admin import credentials
 import os
+import time
 
 db_host = os.getenv('DB_HOST')
 db_port = os.getenv('DB_PORT')
@@ -18,43 +19,46 @@ firebase_admin.initialize_app(cred)
 
 db_con = psycopg2.connect(dbname=db_name, user=db_user, host=db_host, port=db_port, password=db_pass)
 cur = db_con.cursor()
-#  check for live leases
-# compare the end times to now
 
-lease_query = """SELECT *
-    FROM leases
-    WHERE (
-        (time_of < @new_time_to AND time_to > @new_time_of)
-    ) AND WHERE status = true
-    LIMIT 1; """
 
-cur.execute(lease_query, (datetime.now()))
-active_leases = cur.fetchall()
-# if its over the limit get user email
-for i in active_leases:
-    email_query = "SELECT email FROM USER WHERE id_user = %s"
-    cur.execute(email_query, (i))
-    email = cur.fetchone()
+while True:
+    lease_query = """SELECT *
+        FROM leases
+        WHERE (
+            (time_of < @new_time_to AND time_to > @new_time_of)
+        ) AND WHERE status = true
+        LIMIT 1; """
 
-    # send notif to the email topic and the
-    str_mess = "Skončil sa limit na vrátenie auta, prosím odovzdajte auto v aplikácií!"
+    cur.execute(lease_query, (datetime.now()))
+    active_leases = cur.fetchall()
 
-    #TODO: PRED POSLANIM SA MUSIM ZBAVIT @ V EMAILE KEDZE TO NENI PLATNY TOPIC, NAHRAD HO _
-    #! email.replace("@", "_")
-    message = messaging.Message(
-                        notification=messaging.Notification(
-                        title="Prekrocenie limitu na odovzdanie auta",
-                        body=str_mess
-                    ),
-                        topic=email[0].replace("@", "_")
-                    )
-    
-    messaging.send(message)
+    # if its over the limit get user email
+    for i in active_leases:
+        email_query = "SELECT email FROM USER WHERE id_user = %s"
+        cur.execute(email_query, (i))
+        email = cur.fetchone()
 
-    message = messaging.Message(
-        notification=messaging.Notification(
-            title="Zamestnanec {} neskoro odovzdal auto {}.",
-            body="Zamestnanec {} neskoro odovzdal auto {} o {} minút."
-        ),
-        topic = "manager"
-    )
+        # send notif to the email topic and the
+        str_mess = "Skončil sa limit na vrátenie auta, prosím odovzdajte auto v aplikácií!"
+
+        #TODO: PRED POSLANIM SA MUSIM ZBAVIT @ V EMAILE KEDZE TO NENI PLATNY TOPIC, NAHRAD HO _
+        #! email.replace("@", "_")
+        message = messaging.Message(
+                            notification=messaging.Notification(
+                            title="Prekrocenie limitu na odovzdanie auta",
+                            body=str_mess
+                        ),
+                            topic=email[0].replace("@", "_")
+                        )
+        
+        messaging.send(message)
+
+        message = messaging.Message(
+            notification=messaging.Notification(
+                title="Zamestnanec {} neskoro odovzdal auto {}.",
+                body="Zamestnanec {} neskoro odovzdal auto {} o {} minút."
+            ),
+            topic = "manager"
+        )
+        
+    time.sleep(1700)
