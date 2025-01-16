2787ff0338a04d973218ed256fdd7552ca82dac5
Devlogs <102648064+IndexProselyte@users.noreply.github.com>
2025-01-16 13:24:21 +0100

    Update app.py

--- api/app.py
+++ api/app.py
@@ -685,17 +685,12 @@ def lease_car():
 
     except Exception as e:
     # CWD IS /app
-      path = f"{os.getcwd()}/reports/{get_sk_date()} ICLS report.csv"
+      path = f"{os.getcwd()}/{get_sk_date()} ICLS report.csv"
 
       with open(path, "a+") as new_report:
         new_report.write("Meno,Auto,Čas prevziatia,Čas odovzdania,Čas vrátenia,Meškanie,Poznámka")
         new_report.write(f"{recipient},{car_name},{timeof},{timeto},{"REPLACE"},{"REPLACE"}")
-        new_report.close()
 
-    with open(path, "a+") as new_report:
-        new_report.write("Meno,Auto,Čas prevziatia,Čas odovzdania,Čas vrátenia,Meškanie,Poznámka")
-        new_report.write(f"{recipient},{car_name},{timeof},{timeto},{"REPLACE"},{"REPLACE"}")
-        new_report.close()
 
     return {"status": True, "private": private}
       
