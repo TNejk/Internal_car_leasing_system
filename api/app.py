8f6f7926e8c82614bc66ffa1f4599263319a8adc
Devlogs <102648064+IndexProselyte@users.noreply.github.com>
2025-01-29 20:14:04 +0100

    Update app.py

--- api/app.py
+++ api/app.py
@@ -178,8 +178,10 @@ def register():
   hashed = hashlib.sha256(salted.encode()).hexdigest()
 
 
-  result = cur.execute("insert into driver(name, password, role) values( %s, %s, %s,") ,(email, salted, role, )
-
+  result = cur.execute(
+      "INSERT INTO driver (name, password, role) VALUES (%s, %s, %s)",
+      (email, salted, role)
+  )
   if result:
     return {"status": True}
   else:
