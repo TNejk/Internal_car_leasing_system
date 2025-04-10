02224aeb7e3080247cb8d3f33df0ba74d19028e0
TNejk <100270199+TNejk@users.noreply.github.com>
2025-04-10 13:31:03 +0200

    wtfffffffffffffffff

--- misc/load_icons.py
+++
@@ -1,9 +0,0 @@
-from flask import url_for
-def load_icons():
-  bell = url_for('static', filename='sources/images/bell.svg')
-  user = url_for('static', filename='sources/images/user.svg')
-  settings = url_for('static', filename='sources/images/settings.svg')
-  house = url_for('static', filename='sources/images/house.svg')
-  car = url_for('static', filename='sources/images/car.svg')
-  icons = [bell, user, settings, house, car]
-  return icons
--- static/js/script.js
+++
@@ -1,6 +0,0 @@
-function add_notification(title, content, route){
-  const date = new Date().toISOString();
-  const formattedDate = date.toISOString().replace('T', ' ').replace('Z', '') + "+0000";
-  body = {'seen': false, 'title': title, 'content': content, 'time': formattedDate, 'route': route};
-  fetch('/save_notification', {method: 'POST', body: JSON.stringify(formattedDate)})
-}
