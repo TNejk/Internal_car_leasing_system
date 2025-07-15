72778c69c65fb05902a637b0599bd42bd4b38378
TNejk <100270199+TNejk@users.noreply.github.com>
2025-07-15 14:48:18 +0200

    fix

--- api/routers/auth.py
+++ api/routers/auth.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, HTTPException, Depends, status
 from fastapi.security import OAuth2PasswordRequestForm
 from typing import Annotated
-from internal.dependencies import get_current_user, connect_to_db
+from internal.dependencies import get_current_user, connect_to_db, authenticate_user
 import api_models.default as modef
 import api_models.request as moreq
 import api_models.response as mores
@@ -10,13 +10,13 @@ from sqlalchemy.orm import Session
 router = APIRouter(prefix='/v2/auth', tags=['auth'])
 TOKEN_EXPIRATION_MINUTES = 30
 
-@router.post("/logout/", response_model=modef.DefaultResponse)
+@router.post("/logout", response_model=modef.DefaultResponse)
 async def logout(current_user: Annotated[modef.User, Depends(get_current_user)]):
   """Logout endpoint to revoke JWT token"""
   pass
 
 
-@router.post("/register/", response_model=modef.DefaultResponse)
+@router.post("/register", response_model=modef.DefaultResponse)
 async def register(request: moreq.UserRegister, current_user: Annotated[modef.User, Depends(get_current_user)]):
   """Register a new user (admin only)"""
 
@@ -31,10 +31,10 @@ async def register(request: moreq.UserRegister, current_user: Annotated[modef.Us
   pass
 
 
-@router.post("/login/", response_model=mores.LoginResponse)
+@router.post("/login", response_model=mores.LoginResponse)
 async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Session = Depends(connect_to_db)):
   """ Login user to app, returns a login_response obj that includes a token and role email combo. """
-  user = depend.auth.authenticate_user(
+  user = authenticate_user(
     email=form_data.username,
     password=form_data.password,
     db=db
